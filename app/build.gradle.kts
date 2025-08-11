b55473ee9b3a37c27f5fba436a21d4a20e99256e
Muhammad <mohamedkam000@gmail.com>
2025-08-11 23:24:27 +0200

    release 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
     implementation("androidx.activity:activity-compose:1.12.0-alpha05")
     implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.compose.runtime:runtime:1.9.0-rc01")
     implementation("androidx.compose.material3:material3:1.5.0-alpha01")
     implementation("androidx.navigation:navigation-compose:2.9.2")
