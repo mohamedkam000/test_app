2059382a4c922baea011e4b0b84b99eb9ffb40d4
Muhammad <mohamedkam000@gmail.com>
2025-08-11 20:04:29 +0200

    fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
         }
     }
@@ -44,9 +45,9 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.15"
+//    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_19
@@ -58,14 +59,14 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.07.00"))
     implementation("androidx.compose.ui:ui:1.9.0-rc01")
     implementation("androidx.compose.animation:animation:1.9.0-rc01")
-//    implementation("androidx.compose.ui:ui-tooling:1.9.0-rc01")
-//    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.9.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
     implementation("androidx.activity:activity-compose:1.12.0-alpha05")
     implementation("androidx.compose.material:material-icons-core:1.7.8")
     implementation("androidx.compose.runtime:runtime:1.9.0-rc01")
     implementation("androidx.compose.material3:material3:1.5.0-alpha01")
     implementation("androidx.navigation:navigation-compose:2.9.2")
-//    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.14.0-alpha03")
     implementation("androidx.activity:activity-ktx:1.12.0-alpha05")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0-alpha01")
