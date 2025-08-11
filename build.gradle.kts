c2fcef8355630dd9901f3603b915473e8d17be16
Muhammad <mohamedkam000@gmail.com>
2025-08-11 11:14:45 +0200

    test build

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.application") version "8.13.0-alpha03" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20-Beta2" apply false
+    id("com.android.library") version "8.13.0-alpha03" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20-Beta2" apply false
+}
