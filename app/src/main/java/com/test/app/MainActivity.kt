546d7b07661fac205ff02eac130f4c45359aec97
Muhammad <mohamedkam000@gmail.com>
2025-08-12 08:17:39 +0200

    upload

--- app/src/main/java/com/test/app/MainActivity.kt
+++ app/src/main/java/com/test/app/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.*
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
