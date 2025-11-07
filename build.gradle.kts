b05b9739c923aedfade8e1a7cbf4034f6fe09344
Muhammad <mohamedkam000@gmail.com>
2025-11-07 12:03:43 +0000

    oof

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.13.0-alpha03" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20-Beta2" apply false
+    id("com.google.devtools.ksp") version "2.3.1" apply false
     id("com.android.library") version "8.13.0-alpha03" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20-Beta2" apply false
 }
